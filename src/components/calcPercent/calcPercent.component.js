117f470c1163c8c93d9603b63e33d354f7dfed50
Wojciech Zając <wojciechzajaaac@gmail.com>
2018-12-05 21:50:23 +0100

    Use HTML attribute in component template

--- src/components/calcPercent/calcPercent.component.js
+++ src/components/calcPercent/calcPercent.component.js
@@ -1,6 +1,7 @@
 angular.module('components')
     .component('calcPercent', {
+        bindings: { tax: '@' },
         template: '<label>Price' +
         '<input ng-model="price" type="number"></label>' +
-        '<p>Price with 18% tax: {{ price + (price * 18 / 100) }}</p>'
+        '<p>Price with 18% tax: {{ price + (price * $ctrl.tax / 100) }}</p>'
     })
--- src/components/calcPercentRange/calcPercentRange.component.js
+++ src/components/calcPercentRange/calcPercentRange.component.js
@@ -1,6 +1,7 @@
 angular.module('components')
     .component('calcPercentRange', {
+        bindings: { tax: '@' },
         template: '<label>Price: {{price}}' +
         '<input ng-model="price" type="range" min="0" step="1"></label>' +
-        '<p>Price with 32% tax: {{ price + (price * 32 / 100) }}</p>'
+        '<p>Price with 32% tax: {{ price + (price * $ctrl.tax / 100) }}</p>'
     })
